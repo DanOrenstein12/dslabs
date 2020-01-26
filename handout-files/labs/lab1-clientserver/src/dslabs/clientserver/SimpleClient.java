6fb35854f3475970e6b05e14c6a20e07cd3f9adf
DanOrenstein12 <38895785+DanOrenstein12@users.noreply.github.com>
2020-01-26 17:09:38 -0500

    removing stupidity

--- handout-files/labs/lab1-clientserver/src/dslabs/clientserver/SimpleClient.java
+++ handout-files/labs/lab1-clientserver/src/dslabs/clientserver/SimpleClient.java
@@ -5,7 +5,7 @@ import dslabs.framework.Client;
 import dslabs.framework.Command;
 import dslabs.framework.Node;
 import dslabs.framework.Result;
-import dslabs.kvstore.*;
+import dslabs_new.kvstore.*;
 import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
