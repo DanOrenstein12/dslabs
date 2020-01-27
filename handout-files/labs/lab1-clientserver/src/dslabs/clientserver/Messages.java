184c6eaf60152330706b12257a3d0976ada2c03a
DanOrenstein12 <38895785+DanOrenstein12@users.noreply.github.com>
2020-01-26 19:35:57 -0500

    seeing if its actually gonna compile

--- handout-files/labs/lab1-clientserver/src/dslabs/clientserver/Messages.java
+++ handout-files/labs/lab1-clientserver/src/dslabs/clientserver/Messages.java
@@ -8,11 +8,11 @@ import lombok.Data;
 @Data
 class Request implements Message {
     private final Command command;
-    private final int sequenceNum;
+    public final int sequenceNum;
 }
 
 @Data
 class Reply implements Message {
     private final Result result;
-    private final int sequenceNum;
+    public final int sequenceNum;
 }
