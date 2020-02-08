4cc33eaa68b38ebf93f22f45bdfc03308598e4ad
DanOrenstein12 <38895785+DanOrenstein12@users.noreply.github.com>
2020-02-08 15:23:10 -0500

    lab 2

--- handout-files/labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
+++ handout-files/labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
@@ -2,6 +2,9 @@ package dslabs.primarybackup;
 
 import dslabs.framework.Address;
 import dslabs.framework.Node;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -14,7 +17,13 @@ class ViewServer extends Node {
     private static final int INITIAL_VIEWNUM = 1;
 
     // Your code here...
+    private View view;
+    private boolean acknowledge = false;
+    private HashSet<Address> prevFrame;
+    private HashSet<Address> currFrame;
 
+    static final int STARTER = 0;
+    public static int globalID = STARTER;
     /* -------------------------------------------------------------------------
         Construction and Initialization
        -----------------------------------------------------------------------*/
@@ -26,6 +35,10 @@ class ViewServer extends Node {
     public void init() {
         set(new PingCheckTimer(), PING_CHECK_MILLIS);
         // Your code here...
+        this.view = new View(STARTUP_VIEWNUM, null, null);
+        prevFrame = new HashSet<>();
+        currFrame = new HashSet<>();
+
     }
 
     /* -------------------------------------------------------------------------
@@ -33,10 +46,34 @@ class ViewServer extends Node {
        -----------------------------------------------------------------------*/
     private void handlePing(Ping m, Address sender) {
         // Your code here...
+        this.currFrame.add(sender);
+        if(this.view.primary() == null && this.view.viewNum() == STARTUP_VIEWNUM) {
+            this.view = new View(INITIAL_VIEWNUM, sender, null);
+            acknowledge = false;
+        }
+        if(Objects.equals(sender, this.view.primary()) && m.viewNum() == this.view.viewNum()) {
+            acknowledge = true;
+        }
+        if(acknowledge) {
+//            System.out.println(this.view.viewNum());
+            if(this.view.backup() == null) {
+                if(!Objects.equals(sender, this.view.primary())) {
+                    this.view = new View(this.view.viewNum() + 1, this.view.primary(), sender);
+                    acknowledge = false;
+                } else if(getRealExtra(sender) != null) {
+                    this.view = new View(this.view.viewNum() + 1, this.view.primary(), getRealExtra(sender));
+                    acknowledge = false;
+                }
+
+            }
+        }
+        this.send(new ViewReply(this.view), sender);
+
     }
 
-    private void handleGetView(GetView m, Address sender) {
+    private synchronized void handleGetView(GetView m, Address sender) {
         // Your code here...
+        this.send(new ViewReply(this.view), sender);
     }
 
     /* -------------------------------------------------------------------------
@@ -44,6 +81,24 @@ class ViewServer extends Node {
        -----------------------------------------------------------------------*/
     private void onPingCheckTimer(PingCheckTimer t) {
         // Your code here...
+        this.prevFrame = (HashSet)this.currFrame.clone();
+        this.currFrame = new HashSet<>();
+        if(acknowledge) {
+            if(!isAlive(this.view.primary())) {
+
+                if(isAlive(this.view.backup())) {
+                    this.view = new View(this.view.viewNum() + 1, this.view.backup(), getExtra(this.view.backup(), prevFrame));
+                    acknowledge = false;
+                }
+            }
+            else if(!isAlive(this.view.backup())) {
+//                System.out.println("Im dead");
+//                System.out.println(getExtra(this.view.backup(), prevFrame));
+                this.view = new View(this.view.viewNum() + 1, this.view.primary(), getExtra(this.view.primary(), prevFrame));
+//
+            }
+        }
+
         set(t, PING_CHECK_MILLIS);
     }
 
@@ -51,4 +106,31 @@ class ViewServer extends Node {
         Utils
        -----------------------------------------------------------------------*/
     // Your code here...
+    private boolean isAlive(Address address) {
+        if(currFrame.contains(address) || prevFrame.contains(address)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Address getRealExtra(Address address) {
+        if(!this.currFrame.isEmpty()) {
+            Address a1 = getExtra(address, currFrame);
+            if(a1 != null)
+                return a1;
+        }
+        return getExtra(address, prevFrame);
+    }
+
+    private Address getExtra(Address address, HashSet set) {
+        Iterator<Address> it = set.iterator();
+        while(it.hasNext()) {
+            Address t = it.next();
+            if(!Objects.equals(t, address)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
 }
