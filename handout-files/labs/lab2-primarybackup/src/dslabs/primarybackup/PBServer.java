7386c0debe3ad52e0bc8a575108e1910b1e027f2
DanOrenstein12 <38895785+DanOrenstein12@users.noreply.github.com>
2020-02-10 00:34:38 -0500

    slowly fixing this garbage fire

--- handout-files/labs/lab2-primarybackup/src/dslabs/primarybackup/PBServer.java
+++ handout-files/labs/lab2-primarybackup/src/dslabs/primarybackup/PBServer.java
@@ -115,9 +115,9 @@ class PBServer extends Node {
             throws InterruptedException {
         if (isPrimary) {
             if (hasBackup()) {
-                Request forward_request = new ForwardRequest(m, sender);
+                Message forward_request = new ForwardRequest(m, sender);
                 this.send(forward_request, this.view.backup());
-                this.set(new ForwardRequestTimer(command, request_number),100);
+                this.set(new ForwardRequestTimer(m,100);
                 while (this.recentReply == null) {
                     wait();
                 }
